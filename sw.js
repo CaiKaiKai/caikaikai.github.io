4e14a7b7e293041c42788ef707d17d6f73750c07
CaiKaiKai <872369552@qq.com>
2018-02-26 16:05:47 +0800

    caikaikai

--- sw.js
+++ sw.js
@@ -3,29 +3,27 @@
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
-    	Promise(function(resolve,reject){
-	        caches.open('my-cache-v1').then(function (cache) {
-	            return cache.addAll(
-	                "/",
-					"/assets/css/main.css",
-					"/images/banner.jpg",
-					"/images/spotlight01.jpg",
-					"/images/spotlight02.jpg",
-					"/images/spotlight03.jpg",
-					"/images/gallery/fulls/01.jpg",
-					"/images/gallery/fulls/02.jpg",
-					"/images/gallery/fulls/03.jpg",
-					"/images/gallery/fulls/04.jpg",
-					"/assets/css/font-awesome.min.css",
-					"/assets/js/jquery.min.js",
-					"/assets/js/jquery.scrollex.min.js",
-					"/assets/js/jquery.scrolly.min.js",
-					"/assets/js/skel.min.js",
-					"/assets/js/util.js",
-					"/assets/js/main.js"
-	            );
-	        })
-    	})
+        caches.open('my-cache-v1').then(function (cache) {
+            return cache.addAll([
+                "/",
+				"/assets/css/main.css",
+				"/images/banner.jpg",
+				"/images/spotlight01.jpg",
+				"/images/spotlight02.jpg",
+				"/images/spotlight03.jpg",
+				"/images/gallery/fulls/01.jpg",
+				"/images/gallery/fulls/02.jpg",
+				"/images/gallery/fulls/03.jpg",
+				"/images/gallery/fulls/04.jpg",
+				"/assets/css/font-awesome.min.css",
+				"/assets/js/jquery.min.js",
+				"/assets/js/jquery.scrollex.min.js",
+				"/assets/js/jquery.scrolly.min.js",
+				"/assets/js/skel.min.js",
+				"/assets/js/util.js",
+				"/assets/js/main.js"
+            ]);
+        })
     );
 });
 
